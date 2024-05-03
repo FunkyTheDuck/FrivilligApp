4d1b810f236d2b56f882e36315ee6b4ef0cb216e
FunkyTheDuck <dkjsimon@gmail.com>
2024-05-03 13:25:25 +0200

    Har oprettet controllersne men ikke skills og interests, de mangler stadig at kalde til repos

---
+++ H5MiniSvendeprøveS&M/Controllers/RatingController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApiRepository;
+using FrontendModels;
+
+namespace H5MiniSvendeprøveS_M.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingController : ControllerBase
+    {
+        RatingRepository repo {  get; set; }
+        public RatingController()
+        {
+            repo = new RatingRepository();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRatingAsync(Ratings rating)
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteRatingAsync(int ratingId)
+        {
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUsersRatingsAsync(int userId)
+        {
+            return Ok();
+        }
+    }
+}
